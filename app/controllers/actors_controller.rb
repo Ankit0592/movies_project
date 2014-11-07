0c8075b79ba040c2c9543789b4e649d2db9bf0ee
ankitarora <ankit.arora@addvalsolutions.com>
2014-11-07 11:16:29 +0530

    First commit

---
+++ app/controllers/actors_controller.rb
@@ -0,0 +1,48 @@
+class ActorsController < ApplicationController
+	layout 'actor' 
+	def index
+	 
+		@actors=Actor.all
+	end
+	def new
+		@actor=Actor.new
+		
+	end
+	def create
+	 
+	 	actor=Actor.new(actor_params)
+	 	actor.save
+	 	redirect_to actors_path
+	end	
+  
+  def show
+		
+		@actor=Actor.find_by_id(params[:id])
+	end
+
+
+	def edit
+  	
+	 	if params[:id].present?
+	 		@actor=Actor.find_by_id(params[:id])
+	 	end
+	end		
+	def update
+		actor=Actor.find_by_id(params[:id])
+		actor.update(actor_params)
+		redirect_to actors_path
+	end	
+
+	def destroy
+		
+	 	actor=Actor.find_by_id(params[:id])
+    actor.delete
+    redirect_to actors_path
+  end 
+
+	private
+
+	  def actor_params
+	  	params.require(:actor).permit(:name,:movie)
+	  end
+end	
---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,63 @@
+class MoviesController < ApplicationController
+	layout 'movie'
+	def index
+	 @demo=true
+		@movies=Movie.all
+	end	
+	def show
+		@demo=true
+		@movie=Movie.find_by_id(params[:id])
+	end
+	def welcome
+		@demo=false
+	end		
+	def new
+			 @demo=true
+
+		@movie=Movie.new
+		
+	end
+  def edit
+  		 @demo=true
+
+	 	if params[:id].present?
+	 		@movie=Movie.find_by_id(params[:id])
+	 	end
+	end		
+	def update
+  	 @demo=true
+
+		movie=Movie.find_by_id(params[:id])
+		movie.update(movie_params)
+		redirect_to movies_path
+	end	
+
+	def destroy
+ 	 @demo=true
+		
+	 	movie=Movie.find_by_id(params[:id])
+    movie.delete
+    redirect_to movies_path
+  end 
+ 
+	def create
+	 	 @demo=true
+
+	 	movie=Movie.new(movie_params)
+	 	movie.save
+	 	redirect_to movies_path
+	end	
+	
+	def about
+	 @demo=true
+
+  
+	end	
+
+	 private
+
+	  def movie_params
+	  	params.require(:movie).permit(:name,:year)
+	  end
+			
+end
